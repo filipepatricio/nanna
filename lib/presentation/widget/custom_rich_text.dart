5c80aba6df750c8e79d3be7b85cf1533edbbb7c7
Dawid Krysiński <dawid.krysinski@iteo.com>
2021-08-24 15:57:13 +0200

    Modify markdown_flutter library to use CustomRichText widget

--- lib/presentation/widget/custom_rich_text.dart
+++ lib/presentation/widget/custom_rich_text.dart
@@ -4,9 +4,12 @@ import 'dart:ui';
 import 'package:better_informed_mobile/presentation/style/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 
-class CustomRichText extends HookWidget {
+class CustomRichText extends HookWidget implements RichTextBase {
+  @override
   final TextSpan textSpan;
+
   final bool selectable;
 
   const CustomRichText({
