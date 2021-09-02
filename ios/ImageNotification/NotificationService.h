935692bbe0cb12d0b897c88c3a09e622dad8ed22
Dawid Krysiński <dawid.krysinski@iteo.com>
2021-09-02 13:07:44 +0200

    Add support for notification containing images on iOS

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Dawid Krysiński on 02/09/2021.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
