b592bf923cb0cd6e9011028b8d500210efbe23f7
Dawid Krysiński <dawid.krysinski@iteo.com>
2021-09-08 12:00:11 +0200

    Missing translation

--- lib/presentation/widget/see_all_button.dart
+++ lib/presentation/widget/see_all_button.dart
@@ -1,6 +1,8 @@
+import 'package:better_informed_mobile/exports.dart';
 import 'package:better_informed_mobile/presentation/style/app_dimens.dart';
 import 'package:better_informed_mobile/presentation/style/colors.dart';
 import 'package:better_informed_mobile/presentation/style/typography.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 class SeeAllButton extends StatelessWidget {
@@ -26,7 +28,7 @@ class SeeAllButton extends StatelessWidget {
         ),
         child: Center(
           child: Text(
-            'See All',
+            tr(LocaleKeys.common_seeAll),
             style: AppTypography.systemText.copyWith(height: 1),
           ),
         ),
