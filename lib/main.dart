94843ef1c3059c3c4a10619db27a5b842f672420
Dawid Krysiński <dawid.krysinski@iteo.com>
2021-10-04 09:44:01 +0200

    Lock app in portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:fimber/fimber.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:injectable/injectable.dart';
@@ -18,6 +19,11 @@ const _environmentArgKey = 'env';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   final environment = _getEnvironment();
 
