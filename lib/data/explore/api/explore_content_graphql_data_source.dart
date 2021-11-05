1de2a0a7eab6e352803b9a183e25ff88244f50e7
Dawid Krysiński <dawid.krysinski@iteo.com>
2021-11-05 11:04:54 +0100

    Create data layer for explore data pagination

--- lib/data/explore/api/explore_content_graphql_data_source.dart
+++ lib/data/explore/api/explore_content_graphql_data_source.dart
@@ -1,4 +1,5 @@
 import 'package:better_informed_mobile/data/explore/api/dto/explore_content_dto.dart';
+import 'package:better_informed_mobile/data/explore/api/dto/explore_content_section_dto.dart';
 import 'package:better_informed_mobile/data/explore/api/explore_content_api_data_source.dart';
 import 'package:better_informed_mobile/data/explore/api/explore_content_gql.dart';
 import 'package:better_informed_mobile/data/util/graphql_response_resolver.dart';
@@ -26,5 +27,21 @@ class ExploreContentGraphqlDataSource implements ExploreContentApiDataSource {
     );
 
     return dto ?? (throw Exception('Explore content can not be null'));
+  }
+
+  @override
+  Future<ExploreContentSectionDTO> getPaginatedExploreSection(String id, int limit, int offset) async {
+    final result = await _client.query(
+      QueryOptions(
+        document: ExploreContentGQL.paginated(id, limit, offset),
+      ),
+    );
+
+    final dto = _responseResolver.resolve(
+      result,
+      (raw) => ExploreContentSectionDTO.fromJson(raw),
+    );
+
+    return dto ?? (throw Exception('Explore content section can not be null'));
   }
 }
--- lib/data/explore/explore_content_api_repository.dart
+++ lib/data/explore/explore_content_api_repository.dart
@@ -1,19 +1,50 @@
 import 'package:better_informed_mobile/data/explore/api/explore_content_api_data_source.dart';
 import 'package:better_informed_mobile/data/explore/api/mapper/explore_content_dto_mapper.dart';
+import 'package:better_informed_mobile/data/explore/api/mapper/explore_content_section_dto_mapper.dart';
+import 'package:better_informed_mobile/domain/daily_brief/data/media_item.dart';
 import 'package:better_informed_mobile/domain/explore/data/explore_content.dart';
 import 'package:better_informed_mobile/domain/explore/explore_content_repository.dart';
+import 'package:better_informed_mobile/domain/topic/data/topic.dart';
 import 'package:injectable/injectable.dart';
 
 @LazySingleton(as: ExploreContentRepository)
 class ExploreContentApiRepository implements ExploreContentRepository {
   final ExploreContentApiDataSource _dataSource;
   final ExploreContentDTOMapper _exploreContentDTOMapper;
+  final ExploreContentSectionDTOMapper _exploreContentSectionDTOMapper;
 
-  ExploreContentApiRepository(this._dataSource, this._exploreContentDTOMapper);
+  ExploreContentApiRepository(
+    this._dataSource,
+    this._exploreContentDTOMapper,
+    this._exploreContentSectionDTOMapper,
+  );
 
   @override
   Future<ExploreContent> getExploreContent() async {
     final dto = await _dataSource.getExploreContent();
     return _exploreContentDTOMapper(dto);
+  }
+
+  @override
+  Future<List<MediaItemArticle>> getPaginatedArticles(String sectionId, int limit, int offset) async {
+    final dto = await _dataSource.getPaginatedExploreSection(sectionId, limit, offset);
+    final section = _exploreContentSectionDTOMapper(dto);
+
+    return section.maybeMap(
+      articles: (data) => data.articles,
+      articleWithFeature: (data) => data.articles,
+      orElse: () => [],
+    );
+  }
+
+  @override
+  Future<List<Topic>> getPaginatedTopics(String sectionId, int limit, int offset) async {
+    final dto = await _dataSource.getPaginatedExploreSection(sectionId, limit, offset);
+    final section = _exploreContentSectionDTOMapper(dto);
+
+    return section.maybeMap(
+      topics: (data) => data.topics,
+      orElse: () => [],
+    );
   }
 }
