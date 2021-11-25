73a75e67776dc0cc6b208c9990766a81a6430828
esteban miño <minoesteban1@gmail.com>
2021-11-25 20:02:54 +0000

    Update Relax screen illustration

--- lib/presentation/style/vector_graphics.dart
+++ lib/presentation/style/vector_graphics.dart
@@ -39,7 +39,7 @@ class AppVectorGraphics {
 
   static const String readsExploreError = 'assets/vector_graphics/my_reads_explore_error_ic.svg';
   static const String articleError = 'assets/vector_graphics/article_error_ic.svg';
-
+  static const String relaxCoffee = 'assets/vector_graphics/relax_coffee_ic.svg';
   static const String briefLoading = 'assets/vector_graphics/daily_brief_loading.svg';
 
   static const String arrowRight = 'assets/vector_graphics/arrow_right_ic.svg';
