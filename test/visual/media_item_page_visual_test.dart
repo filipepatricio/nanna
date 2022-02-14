feee4caa633c850b171ac1228951ce620641728b
esteban miño <minoesteban1@gmail.com>
2022-02-14 13:26:31 +0100

    Fix golden image test for media item page - topic page changes

--- test/visual/media_item_page_visual_test.dart
+++ test/visual/media_item_page_visual_test.dart
@@ -1,8 +1,7 @@
 import 'package:better_informed_mobile/presentation/page/media/media_item_page.dart';
-import 'package:better_informed_mobile/presentation/page/todays_topics/article/article_item_view.dart';
 import 'package:better_informed_mobile/presentation/page/topic/topic_page.dart';
-import 'package:better_informed_mobile/presentation/style/app_dimens.dart';
 import 'package:better_informed_mobile/presentation/widget/animated_pointer_down.dart';
+import 'package:better_informed_mobile/presentation/widget/link_label.dart';
 import 'package:better_informed_mobile/presentation/widget/reading_list_cover.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,9 +12,9 @@ void main() {
     await tester.startApp();
     await tester.tap(find.byType(ReadingListCover).first);
     await tester.pumpAndSettle();
-    await tester.drag(find.byType(TopicPage), Offset(0, -AppDimens.topicViewArticleSectionFullHeight * 2));
+    await tester.dragUntilVisible(find.byType(LinkLabel).first, find.byType(TopicPage), const Offset(0, -50));
     await tester.pumpAndSettle();
-    await tester.tap(find.byType(ArticleItemView));
+    await tester.tap(find.byType(LinkLabel).first);
     await tester.pumpAndSettle();
     await tester.matchGoldenFile('media_item_page_(image)');
     await tester.tap(find.byType(AnimatedPointerDown));
