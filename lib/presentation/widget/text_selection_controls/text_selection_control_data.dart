ac74437f7322ba7b33fb7b79afd79b71dd7b22b7
Dawid Krysiński <dawid.krysinski@iteo.com>
2022-02-16 15:28:28 +0100

    Add Share menu item to article markdown content

---
+++ lib/presentation/widget/text_selection_controls/text_selection_control_data.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/widgets.dart';
+
+typedef TextSelectionDelegateAction = Function(
+  TextSelectionDelegate delegate,
+  ClipboardStatusNotifier? clipboardStatus,
+);
+
+typedef TextSelectionControlValidator = bool Function(
+  TextSelectionDelegate delegate,
+);
+
+class TextSelectionControlData {
+  TextSelectionControlData(this.label, this.action);
+
+  final String label;
+  final VoidCallback action;
+}
+
+class DelegateTextSelectionControlData {
+  DelegateTextSelectionControlData(
+    this.label,
+    this.action,
+    this.validator,
+  );
+
+  final String label;
+  final TextSelectionDelegateAction action;
+  final TextSelectionControlValidator validator;
+}
+
+extension DelegateTextSelectionControlDataExtension on List<DelegateTextSelectionControlData> {
+  List<TextSelectionControlData> mapToSelectionData(
+    TextSelectionDelegate delegate,
+    ClipboardStatusNotifier clipboardStatus,
+  ) {
+    return where((element) => element.validator(delegate))
+        .map(
+          (control) => TextSelectionControlData(
+            control.label,
+            () => control.action(delegate, clipboardStatus),
+          ),
+        )
+        .toList();
+  }
+}
