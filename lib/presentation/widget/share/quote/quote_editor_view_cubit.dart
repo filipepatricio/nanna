4db1b4b0bc7bf3a7be860c1880a0c385846109b5
Dawid Krysiński <dawid.krysinski@iteo.com>
2022-02-23 14:05:03 +0100

    Implement quote view that will be generated

--- lib/presentation/widget/share/quote/quote_editor_view_cubit.dart
+++ lib/presentation/widget/share/quote/quote_editor_view_cubit.dart
@@ -1,5 +1,8 @@
 import 'package:better_informed_mobile/domain/daily_brief/data/media_item.dart';
 import 'package:better_informed_mobile/presentation/widget/share/quote/quote_editor_view_state.dart';
+import 'package:better_informed_mobile/presentation/widget/share/quote/quote_view.dart';
+import 'package:better_informed_mobile/presentation/widget/share/share_util.dart';
+import 'package:better_informed_mobile/presentation/widget/share/share_view_image_generator.dart';
 import 'package:bloc/bloc.dart';
 import 'package:injectable/injectable.dart';
 
@@ -12,5 +15,20 @@ class QuoteEditorViewCubit extends Cubit<QuoteEditorViewState> {
     emit(updatedState);
   }
 
-  Future<void> shareImage(MediaItemArticle article, String quote) async {}
+  Future<void> share(MediaItemArticle article, String quote) async {
+    var fixedQuote = quote;
+
+    if (quote.length > 140) {
+      fixedQuote = quote.substring(0, 140) + '...';
+    }
+
+    final generator = ShareViewImageGenerator(
+      () => QuoteView(
+        quote: fixedQuote,
+        article: article,
+        quoteVariantData: state.variants[state.selectedIndex],
+      ),
+    );
+    await shareImage(generator, 'quote_${DateTime.now().millisecondsSinceEpoch}.png', '');
+  }
 }
