444e09c05ecfba455de9c1fccabf8cf820d0a572
Dawid Krysiński <dawid.krysinski@iteo.com>
2022-03-07 10:07:24 +0100

    Remove generic sharing message

--- lib/presentation/widget/share/share_util.dart
+++ lib/presentation/widget/share/share_util.dart
@@ -3,15 +3,6 @@ import 'dart:io';
 import 'package:better_informed_mobile/presentation/widget/share/share_view_image_generator.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:share_plus/share_plus.dart';
-
-Future<void> shareImage(ShareViewImageGenerator generator, String fileName, String shareText) async {
-  final file = await generateShareImage(generator, fileName);
-  await Share.shareFiles(
-    [file.path],
-    text: shareText,
-  );
-}
 
 Future<File> generateShareImage(ShareViewImageGenerator generator, String fileName) async {
   final imageBytes = await generator.generate();
