b35f311e45227d05a355dadec592a3316810e134
Dawid Krysiński <dawid.krysinski@iteo.com>
2022-03-16 15:07:43 +0100

    Connect invite page with api

--- lib/presentation/page/invite_friend/invite_friend_page.dart
+++ lib/presentation/page/invite_friend/invite_friend_page.dart
@@ -1,3 +1,4 @@
+import 'package:better_informed_mobile/domain/invite/data/invite_code.dart';
 import 'package:better_informed_mobile/exports.dart';
 import 'package:better_informed_mobile/presentation/page/invite_friend/invite_friend_page_cubit.dart';
 import 'package:better_informed_mobile/presentation/style/app_dimens.dart';
@@ -5,6 +6,8 @@ import 'package:better_informed_mobile/presentation/style/colors.dart';
 import 'package:better_informed_mobile/presentation/style/typography.dart';
 import 'package:better_informed_mobile/presentation/style/vector_graphics.dart';
 import 'package:better_informed_mobile/presentation/util/cubit_hooks.dart';
+import 'package:better_informed_mobile/presentation/widget/general_error_view.dart';
+import 'package:better_informed_mobile/presentation/widget/loader.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +22,13 @@ class InviteFriendPage extends HookWidget {
     final cubit = useCubit<InviteFriendPageCubit>();
     final state = useCubitBuilder(cubit);
 
+    useEffect(
+      () {
+        cubit.initialize();
+      },
+      [cubit],
+    );
+
     return Scaffold(
       appBar: AppBar(
         foregroundColor: AppColors.textPrimary,
@@ -31,112 +41,161 @@ class InviteFriendPage extends HookWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: AppDimens.l),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Spacer(),
-              Center(
-                child: SvgPicture.asset(AppVectorGraphics.bigGift),
-              ),
-              const SizedBox(height: AppDimens.m),
-              Text(
-                tr(LocaleKeys.inviteFriend_header),
-                textAlign: TextAlign.center,
-                style: AppTypography.h2Jakarta,
-              ),
-              const SizedBox(height: AppDimens.s),
-              Text(
-                tr(
-                  LocaleKeys.inviteFriend_content,
-                  args: [
-                    '3',
-                  ],
-                ),
-                textAlign: TextAlign.center,
-                style: AppTypography.h4Normal,
-              ),
-              const SizedBox(height: AppDimens.xxl),
-              DottedBorder(
-                borderType: BorderType.RRect,
-                radius: const Radius.circular(AppDimens.s),
-                color: AppColors.textPrimary,
-                strokeWidth: 1.5,
-                strokeCap: StrokeCap.round,
-                dashPattern: const [0.2, 6],
-                padding: EdgeInsets.zero,
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: AppDimens.m,
-                  ),
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(AppDimens.s),
-                    ),
-                    color: AppColors.pastelGreen,
-                  ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        'YDG3T5',
-                        style: AppTypography.b1Bold.copyWith(height: 1.5),
-                      ),
-                      const Spacer(),
-                      IconButton(
-                        onPressed: () {},
-                        icon: SvgPicture.asset(AppVectorGraphics.copy),
-                        padding: EdgeInsets.zero,
-                        splashRadius: AppDimens.l,
-                      ),
-                      Text(
-                        tr(LocaleKeys.common_copy),
-                        style: AppTypography.b3Regular.copyWith(height: 1.5),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: AppDimens.m),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: AppColors.textPrimary,
-                    width: 1.0,
-                  ),
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(
-                      AppDimens.s,
-                    ),
-                  ),
-                ),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: AppDimens.m,
-                  vertical: AppDimens.sl,
-                ),
-                child: Center(
-                  child: Text(
-                    tr(LocaleKeys.inviteFriend_inviteAction),
-                    style: AppTypography.h4Bold,
-                  ),
-                ),
-              ),
-              const SizedBox(height: AppDimens.xxl),
-              Text(
-                tr(
-                  LocaleKeys.inviteFriend_usage,
-                  args: [
-                    '2',
-                    '3',
-                  ],
-                ),
-                textAlign: TextAlign.center,
-                style: AppTypography.h4Normal.copyWith(
-                  color: AppColors.textGrey,
-                ),
-              ),
-              const Spacer(flex: 2),
+          child: state.map(
+            loading: (_) => const Loader(
+              color: AppColors.limeGreen,
+            ),
+            idle: (state) => _Idle(
+              inviteCode: state.code,
+              cubit: cubit,
+            ),
+            error: (state) => GeneralErrorView(
+              title: tr(LocaleKeys.commonError_oops),
+              content: tr(LocaleKeys.common_generalError),
+              svgPath: AppVectorGraphics.magError,
+              retryCallback: () => cubit.initialize(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Idle extends StatelessWidget {
+  const _Idle({
+    required this.inviteCode,
+    required this.cubit,
+    Key? key,
+  }) : super(key: key);
+
+  final InviteCode inviteCode;
+  final InviteFriendPageCubit cubit;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Spacer(),
+        Center(
+          child: SvgPicture.asset(AppVectorGraphics.bigGift),
+        ),
+        const SizedBox(height: AppDimens.m),
+        Text(
+          tr(LocaleKeys.inviteFriend_header),
+          textAlign: TextAlign.center,
+          style: AppTypography.h2Jakarta,
+        ),
+        const SizedBox(height: AppDimens.s),
+        Text(
+          tr(
+            LocaleKeys.inviteFriend_content,
+            args: [
+              inviteCode.maxUseCount.toString(),
             ],
           ),
+          textAlign: TextAlign.center,
+          style: AppTypography.h4Normal,
+        ),
+        const SizedBox(height: AppDimens.xxl),
+        _CodeContainer(
+          cubit: cubit,
+          inviteCode: inviteCode,
+        ),
+        const SizedBox(height: AppDimens.m),
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: AppColors.textPrimary,
+              width: 1.0,
+            ),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(
+                AppDimens.s,
+              ),
+            ),
+          ),
+          padding: const EdgeInsets.symmetric(
+            horizontal: AppDimens.m,
+            vertical: AppDimens.sl,
+          ),
+          child: Center(
+            child: Text(
+              tr(LocaleKeys.inviteFriend_inviteAction),
+              style: AppTypography.h4Bold,
+            ),
+          ),
+        ),
+        const SizedBox(height: AppDimens.xxl),
+        Text(
+          tr(
+            LocaleKeys.inviteFriend_usage,
+            args: [
+              inviteCode.useCount.toString(),
+              inviteCode.maxUseCount.toString(),
+            ],
+          ),
+          textAlign: TextAlign.center,
+          style: AppTypography.h4Normal.copyWith(
+            color: AppColors.textGrey,
+          ),
+        ),
+        const Spacer(flex: 2),
+      ],
+    );
+  }
+}
+
+class _CodeContainer extends StatelessWidget {
+  const _CodeContainer({
+    required this.inviteCode,
+    required this.cubit,
+    Key? key,
+  }) : super(key: key);
+
+  final InviteCode inviteCode;
+  final InviteFriendPageCubit cubit;
+
+  @override
+  Widget build(BuildContext context) {
+    return DottedBorder(
+      borderType: BorderType.RRect,
+      radius: const Radius.circular(AppDimens.s),
+      color: AppColors.textPrimary,
+      strokeWidth: 1.5,
+      strokeCap: StrokeCap.round,
+      dashPattern: const [0.2, 6],
+      padding: EdgeInsets.zero,
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: AppDimens.m,
+        ),
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(AppDimens.s),
+          ),
+          color: AppColors.pastelGreen,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              inviteCode.code,
+              style: AppTypography.b1Bold.copyWith(height: 1.5),
+            ),
+            const Spacer(),
+            IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset(AppVectorGraphics.copy),
+              padding: EdgeInsets.zero,
+              splashRadius: AppDimens.l,
+            ),
+            Text(
+              tr(LocaleKeys.common_copy),
+              style: AppTypography.b3Regular.copyWith(height: 1.5),
+            ),
+          ],
         ),
       ),
     );
--- lib/presentation/page/invite_friend/invite_friend_page_cubit.dart
+++ lib/presentation/page/invite_friend/invite_friend_page_cubit.dart
@@ -1,13 +1,27 @@
+import 'package:better_informed_mobile/domain/invite/use_case/get_invite_code_use_case.dart';
 import 'package:better_informed_mobile/presentation/page/invite_friend/invite_friend_page_state.dart';
 import 'package:bloc/bloc.dart';
+import 'package:fimber/fimber.dart';
 import 'package:injectable/injectable.dart';
 
 @injectable
 class InviteFriendPageCubit extends Cubit<InviteFriendPageState> {
-  InviteFriendPageCubit() : super(InviteFriendPageState.loading());
+  InviteFriendPageCubit(
+    this._getInviteCodeUseCase,
+  ) : super(InviteFriendPageState.loading());
+
+  final GetInviteCodeUseCase _getInviteCodeUseCase;
 
   Future<void> initialize() async {
+    emit(InviteFriendPageState.loading());
     
+    try {
+      final code = await _getInviteCodeUseCase();
+      emit(InviteFriendPageState.idle(code));
+    } catch (e, s) {
+      Fimber.e('Fetching invite code failed', ex: e, stacktrace: s);
+      emit(InviteFriendPageState.error());
+    }
   }
 
   Future<void> shareCode() async {}
