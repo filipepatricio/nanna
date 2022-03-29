40fa39604a36e5f69cd711599a16f290f9eb4def
Dawid Krysiński <dawid.krysinski@iteo.com>
2022-03-29 15:52:35 +0200

    Code adjustments after rebase

--- lib/data/audio/audio_repository_mock.dart
+++ lib/data/audio/audio_repository_mock.dart
@@ -1,59 +1,35 @@
 import 'package:better_informed_mobile/domain/app_config/app_config.dart';
 import 'package:better_informed_mobile/domain/audio/audio_repository.dart';
 import 'package:better_informed_mobile/domain/audio/data/audio_item.dart';
-import 'package:better_informed_mobile/domain/audio/data/audio_item_metadata.dart';
 import 'package:better_informed_mobile/domain/audio/data/audio_playback_state.dart';
 import 'package:injectable/injectable.dart';
 
 @LazySingleton(as: AudioRepository, env: mockEnvs)
 class AudioRepositoryMock implements AudioRepository {
   @override
-  Future<void> closeItem() {
-    // TODO: implement closeCurrentItem
-    throw UnimplementedError();
-  }
+  Future<void> closeItem() async {}
 
   @override
-  Future<AudioItemMetadata> prepareItem(AudioItem item) {
-    // TODO: implement openItem
-    throw UnimplementedError();
-  }
+  Future<void> prepareItem(AudioItem item) async {}
 
   @override
-  Future<void> pause() {
-    // TODO: implement pause
-    throw UnimplementedError();
-  }
+  Future<void> pause() async {}
 
   @override
-  Future<void> play() {
-    // TODO: implement play
-    throw UnimplementedError();
-  }
+  Future<void> play() async {}
 
   @override
-  // TODO: implement progress
-  Stream<AudioPlaybackState> get playbackState => throw UnimplementedError();
+  Stream<AudioPlaybackState> get playbackState => const Stream.empty();
 
   @override
-  // TODO: implement position
-  Stream<Duration> get position => throw UnimplementedError();
+  Stream<Duration> get position => const Stream.empty();
 
   @override
-  Future<void> seek(Duration seekDuration) {
-    // TODO: implement seek
-    throw UnimplementedError();
-  }
+  Future<void> seek(Duration seekDuration) async {}
 
   @override
-  Future<void> fastForward() {
-    // TODO: implement fastForward
-    throw UnimplementedError();
-  }
+  Future<void> fastForward() async {}
 
   @override
-  Future<void> rewind() {
-    // TODO: implement rewind
-    throw UnimplementedError();
-  }
+  Future<void> rewind() async {}
 }
--- lib/presentation/page/media/widgets/premium_article/premium_article_view.dart
+++ lib/presentation/page/media/widgets/premium_article/premium_article_view.dart
@@ -7,11 +7,6 @@ import 'package:better_informed_mobile/presentation/page/media/widgets/premium_a
 import 'package:better_informed_mobile/presentation/page/media/widgets/premium_article/premium_article_audio_cubit_provider.dart';
 import 'package:better_informed_mobile/presentation/page/media/widgets/premium_article/premium_article_audio_view.dart';
 import 'package:better_informed_mobile/presentation/page/media/widgets/premium_article/premium_article_read_view.dart';
-import 'package:better_informed_mobile/presentation/page/media/media_item_page_gesture_manager.dart';
-import 'package:better_informed_mobile/presentation/page/media/widgets/back_to_topic_button.dart';
-import 'package:better_informed_mobile/presentation/page/media/widgets/premium_article/actions_bar.dart';
-import 'package:better_informed_mobile/presentation/page/media/widgets/premium_article/audio_view.dart';
-import 'package:better_informed_mobile/presentation/page/media/widgets/premium_article/read_view.dart';
 import 'package:better_informed_mobile/presentation/widget/snackbar/snackbar_parent_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
