2640a16d4148724ee733eb0b64dbf5eed4939238
Dawid Krysiński <dawid.krysinski@iteo.com>
2022-03-31 12:33:22 +0200

    Add audio file extension

--- lib/data/audio/audio_repository_impl.dart
+++ lib/data/audio/audio_repository_impl.dart
@@ -11,7 +11,7 @@ import 'package:injectable/injectable.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
-const _audioFileName = 'audio_file';
+const _audioFileName = 'audio_file.mp3';
 
 @LazySingleton(as: AudioRepository, env: liveEnvs)
 class AudioRepositoryImpl implements AudioRepository {
