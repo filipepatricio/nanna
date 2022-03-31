05ee0d0b0c0856e81f898eeb8f9b261eef1fb40f
Dawid Krysiński <dawid.krysinski@iteo.com>
2022-03-31 16:17:13 +0200

    Add some audio logic tests

--- lib/data/audio/audio_repository_mock.dart
+++ lib/data/audio/audio_repository_mock.dart
@@ -3,26 +3,54 @@ import 'package:better_informed_mobile/domain/audio/audio_repository.dart';
 import 'package:better_informed_mobile/domain/audio/data/audio_item.dart';
 import 'package:better_informed_mobile/domain/audio/data/audio_playback_state.dart';
 import 'package:injectable/injectable.dart';
+import 'package:rxdart/subjects.dart';
 
 @LazySingleton(as: AudioRepository, env: mockEnvs)
 class AudioRepositoryMock implements AudioRepository {
-  @override
-  Future<void> closeItem() async {}
+  final BehaviorSubject<AudioPlaybackState> _playbackState =
+      BehaviorSubject.seeded(const AudioPlaybackState.notInitialized());
 
   @override
-  Future<void> prepareItem(AudioItem item) async {}
+  Future<void> closeItem() async {
+    _playbackState.add(const AudioPlaybackState.notInitialized());
+  }
 
   @override
-  Future<void> pause() async {}
+  Future<void> prepareItem(AudioItem item) async {
+    _playbackState.add(const AudioPlaybackState.loading(speed: 1.0));
+    _playbackState.add(
+      const AudioPlaybackState.paused(
+        duration: Duration(seconds: 120),
+        speed: 1.0,
+      ),
+    );
+  }
 
   @override
-  Future<void> play() async {}
+  Future<void> pause() async {
+    _playbackState.add(
+      const AudioPlaybackState.paused(
+        duration: Duration(seconds: 120),
+        speed: 1.0,
+      ),
+    );
+  }
 
   @override
-  Stream<AudioPlaybackState> get playbackState => const Stream.empty();
+  Future<void> play() async {
+    _playbackState.add(
+      const AudioPlaybackState.playing(
+        duration: Duration(seconds: 120),
+        speed: 1.0,
+      ),
+    );
+  }
 
   @override
-  Stream<Duration> get position => const Stream.empty();
+  Stream<AudioPlaybackState> get playbackState => _playbackState.stream;
+
+  @override
+  Stream<Duration> get position => Stream.value(const Duration(seconds: 10));
 
   @override
   Future<void> seek(Duration seekDuration) async {}
