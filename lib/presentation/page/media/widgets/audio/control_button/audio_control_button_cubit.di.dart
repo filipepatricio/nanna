5c1e61d81dc0b5097c39c05d8c0f7b9ff8ce01dc
filipepatricio <facpatricio@gmail.com>
2022-04-04 17:32:41 +0100

    Add article audio events tracking

--- lib/presentation/page/media/widgets/audio/control_button/audio_control_button_cubit.di.dart
+++ lib/presentation/page/media/widgets/audio/control_button/audio_control_button_cubit.di.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 
+import 'package:better_informed_mobile/domain/analytics/analytics_event.dt.dart';
+import 'package:better_informed_mobile/domain/analytics/use_case/track_activity_use_case.di.dart';
 import 'package:better_informed_mobile/domain/audio/use_case/audio_playback_state_stream_use_case.di.dart';
+import 'package:better_informed_mobile/domain/audio/use_case/audio_position_stream_use_case.di.dart';
 import 'package:better_informed_mobile/domain/audio/use_case/pause_audio_use_case.di.dart';
 import 'package:better_informed_mobile/domain/audio/use_case/play_audio_use_case.di.dart';
 import 'package:better_informed_mobile/domain/audio/use_case/prepare_audio_track_use_case.di.dart';
@@ -16,18 +19,27 @@ class AudioControlButtonCubit extends Cubit<AudioControlButtonState> {
     this._pauseAudioUseCase,
     this._audioPlaybackStateStreamUseCase,
     this._prepareAudioTrackUseCase,
+    this._trackActivityUseCase,
+    this._audioPositionStreamUseCase,
   ) : super(AudioControlButtonState.loading());
 
   final PrepareArticleAudioTrackUseCase _prepareAudioTrackUseCase;
   final PlayAudioUseCase _playAudioUseCase;
   final PauseAudioUseCase _pauseAudioUseCase;
   final AudioPlaybackStateStreamUseCase _audioPlaybackStateStreamUseCase;
+  final AudioPositionStreamUseCase _audioPositionStreamUseCase;
+  final TrackActivityUseCase _trackActivityUseCase;
+
+  late MediaItemArticle _article;
+  late bool _isArticleListened;
 
   StreamSubscription? _audioPlaybackSubscription;
+  StreamSubscription? _audioProgressSubscription;
 
   @override
   Future<void> close() async {
     await _audioPlaybackSubscription?.cancel();
+    await _audioProgressSubscription?.cancel();
     return super.close();
   }
 
@@ -42,6 +54,13 @@ class AudioControlButtonCubit extends Cubit<AudioControlButtonState> {
       );
       emit(state);
     });
+    _isArticleListened = false;
+    _audioProgressSubscription = _audioPositionStreamUseCase().listen((event) {
+      if (event.position.inSeconds / event.totalDuration.inSeconds > 0.9 && !_isArticleListened) {
+        _isArticleListened = true;
+        _trackActivityUseCase.trackEvent(AnalyticsEvent.listenedToArticleAudio(_article.id));
+      }
+    });
   }
 
   Future<void> play(MediaItemArticle article, String? imageUrl) async {
@@ -55,9 +74,12 @@ class AudioControlButtonCubit extends Cubit<AudioControlButtonState> {
       },
       paused: (_) => _playAudioUseCase(),
     );
+    _article = article;
+    _trackActivityUseCase.trackEvent(AnalyticsEvent.playedArticleAudio(_article.id));
   }
 
   Future<void> pause() async {
     await _pauseAudioUseCase();
+    _trackActivityUseCase.trackEvent(AnalyticsEvent.pausedArticleAudio(_article.id));
   }
 }
