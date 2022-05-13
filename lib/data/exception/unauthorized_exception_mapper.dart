82f2dbb8180debb3ab8c9da76b86f58309f22739
Dawid Krysinski <dawid.krysinski@iteo.com>
2022-05-13 13:40:42 +0200

    Filter out auth and connection errors

---
+++ lib/data/exception/unauthorized_exception_mapper.dart
@@ -0,0 +1,20 @@
+import 'package:better_informed_mobile/data/exception/exception_mapper.dart';
+import 'package:better_informed_mobile/data/exception/graph_ql_error_extension.dart';
+import 'package:better_informed_mobile/domain/exception/unauthorized_exception.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+class UnauthorizedExceptionMapper extends ExceptionMapper {
+  @override
+  bool isFitting(Object original) {
+    if (original is OperationException) {
+      return original.graphqlErrors.any(_isUnauthenticated);
+    }
+
+    return false;
+  }
+
+  @override
+  Object map(Object original) => UnauthorizedException();
+
+  bool _isUnauthenticated(GraphQLError error) => error.code == 'UNAUTHENTICATED';
+}
