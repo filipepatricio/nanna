830b13e5997b6b818c7e53355b6be43833cddb7d
Dawid Krysinski <dawid.krysinski@iteo.com>
2022-05-25 16:19:50 +0200

    Remove shrinkWrap from topic page

--- lib/presentation/page/topic/topic_view.dart
+++ lib/presentation/page/topic/topic_view.dart
@@ -1,13 +1,17 @@
 import 'package:better_informed_mobile/domain/topic/data/topic.dart';
+import 'package:better_informed_mobile/generated/local_keys.g.dart';
 import 'package:better_informed_mobile/presentation/page/topic/mediaitems/topic_media_items_list.dart';
 import 'package:better_informed_mobile/presentation/page/topic/summary/topic_summary_section.dart';
 import 'package:better_informed_mobile/presentation/page/topic/topic_page_cubit.di.dart';
 import 'package:better_informed_mobile/presentation/page/topic/topic_page_state.dt.dart';
 import 'package:better_informed_mobile/presentation/style/app_dimens.dart';
+import 'package:better_informed_mobile/presentation/style/typography.dart';
 import 'package:better_informed_mobile/presentation/util/cubit_hooks.dart';
 import 'package:better_informed_mobile/presentation/widget/track/general_event_tracker/general_event_tracker.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 
 class TopicView extends HookWidget {
   final Topic topic;
@@ -54,24 +58,34 @@ class TopicView extends HookWidget {
       );
     });
 
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+    return MultiSliver(
       children: [
-        TopicSummarySection(
-          topic: topic,
-          summaryCardKey: summaryCardKey,
-        ),
-        GeneralEventTracker(
-          controller: eventController,
-          child: TopicMediaItemsList(
-            pageIndex: pageIndex,
-            topic: topic,
-            cubit: cubit,
-            eventController: eventController,
-            mediaItemKey: pageIndex.value == 0 ? mediaItemKey : null,
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
+              TopicSummarySection(
+                topic: topic,
+                summaryCardKey: summaryCardKey,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: AppDimens.l, vertical: AppDimens.xl),
+                child: Text(
+                  LocaleKeys.todaysTopics_articlesCount.tr(args: [topic.entries.length.toString()]),
+                  style: AppTypography.h2Regular,
+                  maxLines: 1,
+                ),
+              ),
+            ],
           ),
         ),
+        TopicMediaItemsList(
+          pageIndex: pageIndex,
+          topic: topic,
+          cubit: cubit,
+          eventController: eventController,
+          mediaItemKey: pageIndex.value == 0 ? mediaItemKey : null,
+        ),
+        const SliverPadding(padding: EdgeInsets.only(bottom: AppDimens.xl)),
       ],
     );
   }
