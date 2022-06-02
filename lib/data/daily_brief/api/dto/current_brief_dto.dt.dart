219c06dc425a9fcd08142d7a91ad440c07fc3a82
filipepatricio <facpatricio@gmail.com>
2022-06-02 15:55:38 +0100

    Remove unused import

--- lib/data/daily_brief/api/dto/current_brief_dto.dt.dart
+++ lib/data/daily_brief/api/dto/current_brief_dto.dt.dart
@@ -1,7 +1,6 @@
 import 'package:better_informed_mobile/data/daily_brief/api/dto/brief_entry_dto.dt.dart';
 import 'package:better_informed_mobile/data/daily_brief/api/dto/current_brief_introduction_dto.dt.dart';
 import 'package:better_informed_mobile/data/daily_brief/api/dto/headline_dto.dt.dart';
-import 'package:better_informed_mobile/data/topic/api/dto/topic_dto.dt.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'current_brief_dto.dt.g.dart';
