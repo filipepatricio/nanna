56dc94c02cfb6b1e050bfeb4499be3ac900dd030
lukasz.wojcik <lukasz.wojcik@iteo.com>
2022-07-22 10:44:12 +0200

    feat: Visited state for daily brief covers

--- lib/presentation/widget/article_cover/article_cover.dart
+++ lib/presentation/widget/article_cover/article_cover.dart
@@ -2,6 +2,7 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:better_informed_mobile/domain/daily_brief/data/media_item.dt.dart';
 import 'package:better_informed_mobile/exports.dart';
 import 'package:better_informed_mobile/presentation/page/media/article/article_image.dart';
+import 'package:better_informed_mobile/presentation/page/media/widgets/cover_opacity.dart';
 import 'package:better_informed_mobile/presentation/style/app_dimens.dart';
 import 'package:better_informed_mobile/presentation/style/colors.dart';
 import 'package:better_informed_mobile/presentation/style/typography.dart';
--- lib/presentation/widget/article_cover/article_cover_daily_brief_list.dart
+++ lib/presentation/widget/article_cover/article_cover_daily_brief_list.dart
@@ -22,59 +22,62 @@ class _ArticleCoverDailyBriefListItem extends HookWidget {
     return GestureDetector(
       behavior: HitTestBehavior.opaque,
       onTap: onTap,
-      child: Row(
-        children: [
-          _ArticleSquareCover(
-            article: article,
-            coverColor: coverColor,
-            showArticleIndicator: false,
-            dimension: coverSize,
-          ),
-          const SizedBox(width: AppDimens.m),
-          Expanded(
-            child: SizedBox(
-              height: coverSize,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Row(
-                    children: [
-                      PublisherLogo.dark(publisher: article.publisher),
-                      Flexible(
-                        child: Text(
-                          article.publisher.name,
-                          maxLines: 1,
-                          style: AppTypography.caption1Medium.copyWith(color: AppColors.textGrey),
-                          overflow: TextOverflow.ellipsis,
+      child: CoverOpacity.article(
+        article: article,
+        child: Row(
+          children: [
+            _ArticleSquareCover(
+              article: article,
+              coverColor: coverColor,
+              showArticleIndicator: false,
+              dimension: coverSize,
+            ),
+            const SizedBox(width: AppDimens.m),
+            Expanded(
+              child: SizedBox(
+                height: coverSize,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Row(
+                      children: [
+                        PublisherLogo.dark(publisher: article.publisher),
+                        Flexible(
+                          child: Text(
+                            article.publisher.name,
+                            maxLines: 1,
+                            style: AppTypography.caption1Medium.copyWith(color: AppColors.textGrey),
+                            overflow: TextOverflow.ellipsis,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  const Spacer(),
-                  Text(
-                    article.strippedTitle,
-                    maxLines: 2,
-                    style: AppTypography.h5BoldSmall.copyWith(height: 1.25),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  const Spacer(),
-                  Flexible(
-                    child: ArticleDottedInfo(
-                      article: article,
-                      isLight: false,
-                      showLogo: false,
-                      showPublisher: false,
-                      showDate: false,
-                      textStyle: AppTypography.caption1Medium,
-                      color: AppColors.textGrey,
+                      ],
                     ),
-                  ),
-                ],
+                    const Spacer(),
+                    Text(
+                      article.strippedTitle,
+                      maxLines: 2,
+                      style: AppTypography.h5BoldSmall.copyWith(height: 1.25),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const Spacer(),
+                    Flexible(
+                      child: ArticleDottedInfo(
+                        article: article,
+                        isLight: false,
+                        showLogo: false,
+                        showPublisher: false,
+                        showDate: false,
+                        textStyle: AppTypography.caption1Medium,
+                        color: AppColors.textGrey,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
