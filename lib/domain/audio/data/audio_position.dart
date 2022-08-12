f62ec1f5f3db5202993427ad08af8a752047dbf6
JΛNO <lukasjanek99@gmail.com>
2022-08-12 11:04:38 +0200

    feat: created unit test for audio position

--- lib/domain/audio/data/audio_position.dart
+++ lib/domain/audio/data/audio_position.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class AudioPosition {
   AudioPosition({
     required this.audioItemID,
@@ -8,4 +9,16 @@ class AudioPosition {
   final String audioItemID;
   final Duration position;
   final Duration totalDuration;
+
+  AudioPosition copyWith({
+    String? audioItemID,
+    Duration? position,
+    Duration? totalDuration,
+  }) {
+    return AudioPosition(
+      audioItemID: audioItemID ?? this.audioItemID,
+      position: position ?? this.position,
+      totalDuration: totalDuration ?? this.totalDuration,
+    );
+  }
 }
