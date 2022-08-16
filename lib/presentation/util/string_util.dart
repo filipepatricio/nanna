bb3cdb9d368ea543a549887c7590ec6a25adf04e
lukasz.wojcik <lukasz.wojcik@iteo.com>
2022-08-16 14:17:33 +0200

    fix: Highlight being painted wrong

--- lib/presentation/util/string_util.dart
+++ lib/presentation/util/string_util.dart
@@ -1,4 +1,11 @@
+import 'package:better_informed_mobile/presentation/util/code_unit_util.dart';
+
 extension StringCasingExtension on String {
   String toCapitalized() => length > 0 ? '${this[0].toUpperCase()}${substring(1).toLowerCase()}' : '';
+
   String toTitleCase() => replaceAll(RegExp(' +'), ' ').split(' ').map((str) => str.toCapitalized()).join(' ');
+}
+
+extension StringComparisionExtenion on String {
+  bool get endsWithWhiteSpace => CodeUnitUtil.isWhitespace(codeUnitAt(length - 1));
 }
