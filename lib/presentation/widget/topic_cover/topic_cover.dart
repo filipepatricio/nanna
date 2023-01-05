83daf40436e8db06abefcc90741a9e2f9b42de18
Filipe Patricio <facpatricio@gmail.com>
2023-01-05 15:13:33 +0000

    feat: Adjust and refactor OwnersNote

--- lib/presentation/widget/topic_cover/topic_cover.dart
+++ lib/presentation/widget/topic_cover/topic_cover.dart
@@ -7,7 +7,7 @@ import 'package:better_informed_mobile/presentation/widget/bookmark_button/bookm
 import 'package:better_informed_mobile/presentation/widget/curation/curation_info_view.dart';
 import 'package:better_informed_mobile/presentation/widget/informed_markdown_body.dart';
 import 'package:better_informed_mobile/presentation/widget/informed_pill.dart';
-import 'package:better_informed_mobile/presentation/widget/owners_note_container.dart';
+import 'package:better_informed_mobile/presentation/widget/owners_note.dart';
 import 'package:better_informed_mobile/presentation/widget/publisher_logo_row.dart';
 import 'package:better_informed_mobile/presentation/widget/topic_cover/image/topic_image.dart';
 import 'package:better_informed_mobile/presentation/widget/topic_cover/topic_square_image_frame.dart';
