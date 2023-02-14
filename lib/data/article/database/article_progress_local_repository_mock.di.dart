2896a0de0f64b7948f377b8e5bc8793acc260ab3
Dawid Krysinski <dawid.krysinski@iteo.com>
2023-02-14 09:32:35 +0100

    feat: Enable local storage for offline mode

--- lib/data/article/database/article_progress_local_repository_mock.di.dart
+++ lib/data/article/database/article_progress_local_repository_mock.di.dart
@@ -1,10 +1,10 @@
+import 'package:better_informed_mobile/domain/app_config/app_config.dart';
 import 'package:better_informed_mobile/domain/article/article_progress_local_repository.dart';
 import 'package:better_informed_mobile/domain/article/data/article_progress.dart';
 import 'package:better_informed_mobile/domain/synchronization/synchronizable.dt.dart';
 import 'package:injectable/injectable.dart';
 
-// @LazySingleton(as: ArticleLocalRepository, env: mockEnvs)
-@LazySingleton(as: ArticleProgressLocalRepository)
+@LazySingleton(as: ArticleProgressLocalRepository, env: mockEnvs)
 class ArticleProgressLocalRepositoryMock implements ArticleProgressLocalRepository {
   @override
   Future<void> delete(String id) async {}
