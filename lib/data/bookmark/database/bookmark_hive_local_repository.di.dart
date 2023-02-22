909683100f3125f982b3a6591dd982e9fe3a7ec6
esteban miño <minoesteban1@gmail.com>
2023-02-22 14:31:48 +0100

    feat: Fetch local bookmark state when offline

--- lib/data/bookmark/database/bookmark_hive_local_repository.di.dart
+++ lib/data/bookmark/database/bookmark_hive_local_repository.di.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: unused_field, unused_element
-
 import 'package:better_informed_mobile/data/bookmark/database/entity/bookmark_entity.hv.dart';
 import 'package:better_informed_mobile/data/bookmark/database/entity/bookmark_sort_config_name_entity.dart';
 import 'package:better_informed_mobile/data/bookmark/database/entity/synchronizable_bookmark_entity.hv.dart';
@@ -9,10 +7,12 @@ import 'package:better_informed_mobile/data/synchronization/database/entity/sync
 import 'package:better_informed_mobile/domain/bookmark/bookmark_local_repository.dart';
 import 'package:better_informed_mobile/domain/bookmark/data/bookmark.dart';
 import 'package:better_informed_mobile/domain/bookmark/data/bookmark_sort_config.dart';
+import 'package:better_informed_mobile/domain/bookmark/data/bookmark_state.dt.dart';
 import 'package:better_informed_mobile/domain/synchronization/synchronizable.dt.dart';
 import 'package:hive/hive.dart';
 import 'package:rxdart/rxdart.dart';
 
+const _bookmarkedSlugsBoxName = 'bookmark_slugs';
 const _miscellaneousBoxName = 'bookmark_sort';
 const _sortKey = 'entity';
 const _synchronizationTimeKey = 'synchronization_time';
@@ -25,6 +25,7 @@ class BookmarkHiveLocalRepository implements BookmarkLocalRepository {
     this._synchronizableBookmarkEntityMapper,
     this._bookmarkBox,
     this._miscellaneousBox,
+    this._bookmarkedSlugsBox,
   );
 
   final BookmarkSortConfigNameEntityMapper _bookmarkSortConfigNameEntityMapper;
@@ -32,6 +33,7 @@ class BookmarkHiveLocalRepository implements BookmarkLocalRepository {
 
   final LazyBox<SynchronizableEntity<BookmarkEntity>> _bookmarkBox;
   final Box<String> _miscellaneousBox;
+  final Box<String> _bookmarkedSlugsBox;
 
   static Future<BookmarkHiveLocalRepository> create(
     BookmarkSortConfigNameEntityMapper bookmarkSortConfigNameEntityMapper,
@@ -39,11 +41,13 @@ class BookmarkHiveLocalRepository implements BookmarkLocalRepository {
   ) async {
     final bookmarkBox = await Hive.openLazyBox<SynchronizableEntity<BookmarkEntity>>(_bookmarkBoxName);
     final sortBox = await Hive.openBox<String>(_miscellaneousBoxName);
+    final bookmarkedSlugsBox = await Hive.openBox<String>(_bookmarkedSlugsBoxName);
     return BookmarkHiveLocalRepository._(
       bookmarkSortConfigNameEntityMapper,
       synchronizableBookmarkEntityMapper,
       bookmarkBox,
       sortBox,
+      bookmarkedSlugsBox,
     );
   }
 
@@ -76,6 +80,10 @@ class BookmarkHiveLocalRepository implements BookmarkLocalRepository {
 
   @override
   Future<void> delete(String id) async {
+    final bookmark = await load(id);
+    final slug = bookmark?.data?.slug;
+    if (slug != null) await _bookmarkedSlugsBox.delete(slug);
+
     await _bookmarkBox.delete(id);
   }
 
@@ -91,6 +99,9 @@ class BookmarkHiveLocalRepository implements BookmarkLocalRepository {
   Future<void> save(Synchronizable<Bookmark> bookmark) async {
     final entity = _synchronizableBookmarkEntityMapper.from(bookmark);
     await _bookmarkBox.put(entity.dataId, entity);
+
+    final slug = bookmark.data?.slug;
+    if (slug != null) await _bookmarkedSlugsBox.put(bookmark.data?.slug, entity.dataId);
   }
 
   @override
@@ -104,6 +115,14 @@ class BookmarkHiveLocalRepository implements BookmarkLocalRepository {
   }
 
   @override
+  Future<BookmarkState> getBookmarkState(String slug) async {
+    final bookmarkId = _bookmarkedSlugsBox.get(slug);
+
+    if (bookmarkId != null) return BookmarkState.bookmarked(bookmarkId);
+    return BookmarkState.notBookmarked();
+  }
+
+  @override
   Future<List<String>> getAllIds() async {
     return _bookmarkBox.keys.cast<String>().toList();
   }
@@ -113,4 +132,11 @@ class BookmarkHiveLocalRepository implements BookmarkLocalRepository {
     await _miscellaneousBox.clear();
     await _bookmarkBox.clear();
   }
+}
+
+extension on Bookmark {
+  String? get slug => data.mapOrNull(
+        article: (article) => article.article.slug,
+        topic: (topic) => topic.topic.slug,
+      );
 }
