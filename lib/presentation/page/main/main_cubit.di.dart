ec111487f8486f371cd1111908be02e3dd817149
Dawid Krysinski <dawid.krysinski@iteo.com>
2023-03-20 13:59:03 +0100

    feat: Refresh app when coming back from background after 10 minutes

--- lib/presentation/page/main/main_cubit.di.dart
+++ lib/presentation/page/main/main_cubit.di.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:better_informed_mobile/domain/auth/use_case/get_token_expiration_stream_use_case.di.dart';
+import 'package:better_informed_mobile/domain/daily_brief/use_case/notify_brief_use_case.di.dart';
 import 'package:better_informed_mobile/domain/deep_link/use_case/subscribe_for_deep_link_use_case.di.dart';
 import 'package:better_informed_mobile/domain/feature_flags/use_case/use_paid_subscription_change_stream_use_case.di.dart';
 import 'package:better_informed_mobile/domain/language/language_code.dart';
@@ -10,13 +11,17 @@ import 'package:better_informed_mobile/domain/push_notification/use_case/maybe_r
 import 'package:better_informed_mobile/domain/release_notes/use_case/get_current_release_note_use_case.di.dart';
 import 'package:better_informed_mobile/domain/synchronization/use_case/initialize_synchronization_engine_use_case.di.dart';
 import 'package:better_informed_mobile/domain/synchronization/use_case/synchronize_on_connection_change_use_case.di.dart';
+import 'package:better_informed_mobile/domain/util/use_case/should_refresh_daily_brief_use_case.di.dart';
 import 'package:better_informed_mobile/exports.dart';
 import 'package:better_informed_mobile/presentation/page/main/main_state.dt.dart';
 import 'package:better_informed_mobile/presentation/routing/main_router.dart';
 import 'package:better_informed_mobile/presentation/util/date_format_util.dart';
 import 'package:bloc/bloc.dart';
+import 'package:clock/clock.dart';
 import 'package:fimber/fimber.dart';
 import 'package:injectable/injectable.dart';
+
+const _backgroundRefreshInterval = Duration(minutes: 10);
 
 @injectable
 class MainCubit extends Cubit<MainState> {
@@ -29,6 +34,8 @@ class MainCubit extends Cubit<MainState> {
     this._usePaidSubscriptionChangeStreamUseCase,
     this._initializeSynchronizationEngineUseCase,
     this._synchronizeOnConnectionChangeUseCase,
+    this._updateBriefNotifierUseCase,
+    this._shouldRefreshDailyBriefUseCase,
   ) : super(const MainState.init());
 
   final GetTokenExpirationStreamUseCase _getTokenExpirationStreamUseCase;
@@ -39,12 +46,16 @@ class MainCubit extends Cubit<MainState> {
   final UsePaidSubscriptionChangeStreamUseCase _usePaidSubscriptionChangeStreamUseCase;
   final InitializeSynchronizationEngineUseCase _initializeSynchronizationEngineUseCase;
   final SynchronizeOnConnectionChangeUseCase _synchronizeOnConnectionChangeUseCase;
+  final UpdateBriefNotifierUseCase _updateBriefNotifierUseCase;
+  final ShouldRefreshDailyBriefUseCase _shouldRefreshDailyBriefUseCase;
 
   StreamSubscription? _incomingPushNavigationSubscription;
   StreamSubscription? _tokenExpirationSubscription;
   StreamSubscription? _deepLinkSubscription;
   StreamSubscription? _usePaidSubscriptionFlagChangeSubscription;
   StreamSubscription? _syncOnConnectionChangeSubscription;
+
+  DateTime? _movedToBackgroundAt;
 
   @override
   Future<void> close() async {
@@ -69,6 +80,26 @@ class MainCubit extends Cubit<MainState> {
     unawaited(_maybeRegisterPushNotificationTokenUseCase());
     unawaited(_getReleaseNote());
     unawaited(_initializeSynchronizationEngine());
+  }
+
+  void appMovedToBackground() {
+    _movedToBackgroundAt ??= clock.now();
+  }
+
+  void appMovedToForeground() {
+    final movedToBackgroundAt = _movedToBackgroundAt;
+    if (movedToBackgroundAt != null) {
+      _movedToBackgroundAt = null;
+
+      final duration = clock.now().difference(movedToBackgroundAt);
+
+      if (duration > _backgroundRefreshInterval) {
+        scheduleMicrotask(() {
+          _shouldRefreshDailyBriefUseCase();
+          _updateBriefNotifierUseCase();
+        });
+      }
+    }
   }
 
   void _subscribeToPushNavigationStream() {
