8016f550b13f9a5d8b027f2fa8024a000c1938f0
esteban miño <minoesteban1@gmail.com>
2023-04-11 11:25:21 +0200

    fix: Set correct SystemUIOverlay based on theme

--- lib/presentation/page/entry/entry_page.dart
+++ lib/presentation/page/entry/entry_page.dart
@@ -1,7 +1,9 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:better_informed_mobile/exports.dart';
 import 'package:better_informed_mobile/presentation/page/entry/entry_page_cubit.di.dart';
 import 'package:better_informed_mobile/presentation/page/entry/entry_page_state.dt.dart';
+import 'package:better_informed_mobile/presentation/style/informed_theme.dart';
 import 'package:better_informed_mobile/presentation/util/cubit_hooks.dart';
 import 'package:better_informed_mobile/presentation/widget/error_view.dart';
 import 'package:better_informed_mobile/presentation/widget/loader.dart';
@@ -41,14 +43,19 @@ class EntryPage extends HookWidget {
       [cubit],
     );
 
-    return Scaffold(
-      body: state.maybeMap(
-        error: (_) => Center(
-          child: ErrorView(
-            retryCallback: () => cubit.initialize(),
+    return AnnotatedRegion(
+      value: AdaptiveTheme.of(context).brightness == Brightness.dark
+          ? InformedTheme.systemUIOverlayStyleDark
+          : InformedTheme.systemUIOverlayStyleLight,
+      child: Scaffold(
+        body: state.maybeMap(
+          error: (_) => Center(
+            child: ErrorView(
+              retryCallback: () => cubit.initialize(),
+            ),
           ),
+          orElse: () => const LoaderLogo(),
         ),
-        orElse: () => const LoaderLogo(),
       ),
     );
   }
--- lib/presentation/page/onboarding/slides/onboarding_publishers_slide.dart
+++ lib/presentation/page/onboarding/slides/onboarding_publishers_slide.dart
@@ -3,7 +3,6 @@ import 'package:better_informed_mobile/exports.dart';
 import 'package:better_informed_mobile/presentation/style/app_dimens.dart';
 import 'package:better_informed_mobile/presentation/style/app_raster_graphics.dart';
 import 'package:better_informed_mobile/presentation/style/colors.dart';
-import 'package:better_informed_mobile/presentation/style/informed_theme.dart';
 import 'package:better_informed_mobile/presentation/style/typography.dart';
 import 'package:flutter/material.dart';
 
@@ -12,62 +11,59 @@ class OnboardingPublishersSlide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion(
-      value: InformedTheme.systemUIOverlayStyleDark,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            height: AppDimens.safeTopPadding(context),
-            color: AppColors.transparent,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          height: AppDimens.safeTopPadding(context),
+          color: AppColors.transparent,
+        ),
+        Expanded(
+          flex: 14,
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Positioned(
+                top: -AppDimens.safeTopPadding(context),
+                bottom: AppDimens.zero,
+                left: AppDimens.zero,
+                right: AppDimens.zero,
+                child: Image.asset(
+                  AppRasterGraphics.onboardingPublishers,
+                  alignment: Alignment.topCenter,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            flex: 14,
-            child: Stack(
-              clipBehavior: Clip.none,
+        ),
+        const SizedBox(height: AppDimens.xl),
+        Expanded(
+          flex: 9,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: AppDimens.l),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Positioned(
-                  top: -AppDimens.safeTopPadding(context),
-                  bottom: AppDimens.zero,
-                  left: AppDimens.zero,
-                  right: AppDimens.zero,
-                  child: Image.asset(
-                    AppRasterGraphics.onboardingPublishers,
-                    alignment: Alignment.topCenter,
-                    fit: BoxFit.cover,
-                  ),
+                AutoSizeText(
+                  context.l10n.onboarding_headerSlideOne,
+                  style: AppTypography.onBoardingHeader.copyWith(height: 1.14),
+                  maxLines: 3,
+                  stepGranularity: 0.1,
+                ),
+                const SizedBox(height: AppDimens.s),
+                AutoSizeText(
+                  context.l10n.onboarding_descriptionSlideOne,
+                  style: AppTypography.b2Regular,
+                  maxLines: 4,
+                  stepGranularity: 0.1,
                 ),
               ],
             ),
           ),
-          const SizedBox(height: AppDimens.xl),
-          Expanded(
-            flex: 9,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: AppDimens.l),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  AutoSizeText(
-                    context.l10n.onboarding_headerSlideOne,
-                    style: AppTypography.onBoardingHeader.copyWith(height: 1.14),
-                    maxLines: 3,
-                    stepGranularity: 0.1,
-                  ),
-                  const SizedBox(height: AppDimens.s),
-                  AutoSizeText(
-                    context.l10n.onboarding_descriptionSlideOne,
-                    style: AppTypography.b2Regular,
-                    maxLines: 4,
-                    stepGranularity: 0.1,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
